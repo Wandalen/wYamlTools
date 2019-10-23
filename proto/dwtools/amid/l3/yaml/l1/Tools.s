9d90f4cd6071640c82c40dd65a85f65a6521033a
Wandalen <wandalen.me@gmail.com>
2019-10-23 19:10:49 +0300

    add code

--- proto/dwtools/amid/l3/yaml/l1/Tools.s
+++ proto/dwtools/amid/l3/yaml/l1/Tools.s
@@ -10,11 +10,87 @@ if( typeof module !== 'undefined' )
 }
 
 let _ = wTools;
-let Self = _.npm = _.npm || Object.create( null );
+let Self = _.yaml = _.yaml || Object.create( null );
 
 // --
 // inter
 // --
+
+function lineFind( src, ins )
+{
+
+  let r = Object.create( null );
+  r.regexp = new RegExp( '\\n(((?!\\n)\\s)*)(' + _.regexpEscape( ins ) + ')(.*?)\\n' );
+  let match = src.match( r.regexp );
+
+  if( !match )
+  return null;
+
+  r.line = match[ 0 ].replace( '\n', '' ).replace( '\n', '' );
+  r.pre = match[ 1 ];
+  r.before = src.substring( 0, match.index );
+  r.after = src.substring( match.index + match[ 0 ].length );
+
+  return r;
+}
+
+//
+
+function commentOut( content, name )
+{
+  let regexp = new RegExp( '\\n(((?!\\n)\\s)*)(' + _.regexpEscape( name ) + ')(\\s*:.*?)\\n' );
+
+  let match = content.match( regexp );
+  if( !match )
+  return false;
+
+  let before = content.substring( 0, match.index );
+  let after = content.substring( match.index + match[ 0 ].length );
+  let inside = match[ 0 ].replace( '\n', '' ).replace( '\n', '' );
+  let pre = match[ 1 ];
+  let result = before;
+
+  add( inside );
+
+  after = after.split( '\n' );
+
+  while( after.length )
+  {
+    let line = after[ 0 ];
+    after.splice( 0, 1 );
+    if( !line.trim().length )
+    {
+      add( line );
+      continue;
+    }
+    if( line.length <= pre.length )
+    {
+      close( line );
+      break;
+    }
+    if( line.charCodeAt( pre.length+1 ) > 32 )
+    {
+      close( line );
+      break;
+    }
+    add( line );
+  }
+
+  result += after.join( '\n' );
+
+  return result;
+
+  function close( line )
+  {
+    result += '\n' + line + '\n';
+  }
+
+  function add( line )
+  {
+    line = line.replace( /^\s*/, '' );
+    result += `\n${pre}# ${line}`;
+  }
+}
 
 // --
 // relations
@@ -26,6 +102,9 @@ let Self = _.npm = _.npm || Object.create( null );
 
 let Extend =
 {
+
+  lineFind,
+  commentOut,
 
 }
 
